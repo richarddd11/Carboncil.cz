b394192a8e6a781e9e1fd6b239dff348aad19e71
Richard <richard.palak11@gmail.com>
2025-04-14 17:31:52 +0200

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 
-import NavBar from './components/NavBar'
+import NavBar from './components/NavBar.jsx'
 import HeroSection from './components/HeroSection'
 import ProductSection from './components/ProductSection'
 import FamilySection from './components/FamilySection'
